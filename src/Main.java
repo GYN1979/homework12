6087ffaf1df8d78a153570dbc0ba4afdfc31362f
GYN1979 <galeeva.y@yandex.ru>
2022-10-26 20:22:04 +0400

    Исправленное ДЗ.

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,16 @@
 import java.time.LocalDate;
 
 public class Main {
-
-    //Task
-    public static void printIsLeapYear(int year) {
-        boolean yearIsLeap = isLeap(year);
-        printIsLeapYearResult(year, yearIsLeap);
+    //Task1
+    private static void printyearIsLeap(int year) {
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+        System.out.println (year + " является високосным ");
+    } else {
+        System.out.println(year + " не является високосным");
     }
-
-    private static boolean isLeap(int year) {
-        return year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-    }
-
-    private static void printIsLeapYearResult(int year, boolean yearIsLeap) {
-        if (yearIsLeap) {
-            System.out.println(year + " является високосным ");
-        } else {
-            System.out.println(year + " не является високосным");
-        }
-    }
-
-    //
-    public static void checkingOS(int OS, int deviceYear) {
+}
+    //Task2
+    public static void checkOS(int OS, int deviceYear) {
         int currentYear = 2022;
         if (OS == 0 && deviceYear >= currentYear) {
             System.out.println("Установите версию приложения для IOS по ссылке");
@@ -29,26 +18,27 @@ public class Main {
             System.out.println(" Установите lite версию приложения для IOS по ссылке");
         } else if (OS == 1 && deviceYear >= currentYear) {
             System.out.println(" Установите версию приложения для Android по ссылке");
-        } else if (OS == 1 && deviceYear < currentYear) {
+        } else {
             System.out.println(" Установите lite версию приложения для Android по ссылке");
         }
     }
-
-    //
-    public static void delivery(int deliveryDistance, int daysDelivery) {
-        if (deliveryDistance > 20) {
+    //Task3
+    public static void calculateDeliveryDay (int deliveryDistance, int daysDelivery) {
+        if (deliveryDistance >= 20 && deliveryDistance <60) {
             daysDelivery++;
         }
         if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             daysDelivery++;
-            System.out.println("Потребуется дней: " + daysDelivery);
 
-        }
+        } else if (deliveryDistance > 100) {
+            System.out.println( " Не обслуживаем");
+                    }
+        System.out.println("Потребуется дней: " + daysDelivery);
     }
     public static void main(String[] args){
-        printIsLeapYear(2020);
-        checkingOS(1, 2020);
-        delivery(95, 1);
+        printyearIsLeap (2021);
+        checkOS(1, 2020);
+        calculateDeliveryDay (15,1);
 
     }
 }
