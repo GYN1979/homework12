d03694b3dd49686d70903922cbb2ab5f8370e58c
GYN1979 <galeeva.y@yandex.ru>
2022-10-27 17:53:07 +0400

    Исправленное, в n-й раз ДЗ

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,12 @@ public class Main {
     //Task1
     private static void printyearIsLeap(int year) {
         if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
-        System.out.println (year + " является високосным ");
-    } else {
-        System.out.println(year + " не является високосным");
+            System.out.println(year + " является високосным ");
+        } else {
+            System.out.println(year + " не является високосным");
+        }
     }
-}
+
     //Task2
     public static void checkOS(int OS, int deviceYear) {
         int currentYear = 2022;
@@ -22,26 +23,28 @@ public class Main {
             System.out.println(" Установите lite версию приложения для Android по ссылке");
         }
     }
+
     //Task3
-    public static void calculateDeliveryDay (int deliveryDistance, int daysDelivery) {
-        if (deliveryDistance >= 20 && deliveryDistance <60) {
+    public static void calculateDeliveryDay(int deliveryDistance, int daysDelivery) {
+        if (deliveryDistance >= 20 && deliveryDistance < 60) {
             daysDelivery++;
         }
         if (deliveryDistance >= 60 && deliveryDistance <= 100) {
             daysDelivery++;
+            System.out.println("Потребуется дней: " + daysDelivery);
 
         } else if (deliveryDistance > 100) {
-            System.out.println( " Не обслуживаем");
-                    }
-        System.out.println("Потребуется дней: " + daysDelivery);
+            System.out.println(" Не обслуживаем");
+        }
     }
-    public static void main(String[] args){
-        printyearIsLeap (2021);
-        checkOS(1, 2020);
-        calculateDeliveryDay (15,1);
+        public static void main (String[]args){
+            printyearIsLeap(2021);
+            checkOS(1, 2020);
+            calculateDeliveryDay(110, 1);
 
+        }
     }
-}
+
 
 
 
